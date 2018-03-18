650ba6d7fcb6abc936d0e033e26f7f757c1501cf
ThomasEcalle <thomasecalle@hotmail.fr>
2018-03-18 13:05:31 +0100

    added description and isAccountValid to registration

--- actions/user/register.js
+++ actions/user/register.js
@@ -15,8 +15,10 @@ module.exports = function (req, res) {
     const firstname = req.body.firstname;
     const lastname = req.body.lastname;
     const password = req.body.password;
+    const description = req.body.description;
+    const isAccountValidate = req.body.isAccountValidate;
 
-    userController.register(email, password, lastname, firstname)
+    userController.register(email, password, lastname, firstname, description, isAccountValidate)
         .then((user) => {
             res
                 .status(config.constants.OK)
