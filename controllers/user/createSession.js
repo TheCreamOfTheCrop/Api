bd4abe37584ceec73f9c674d651864c5b04a3559
ThomasEcalle <thomasecalle@hotmail.fr>
2018-04-07 19:36:19 +0200

    removed token's availability

--- controllers/user/createSession.js
+++ controllers/user/createSession.js
@@ -7,7 +7,6 @@ module.exports = function(user){
     const expires = moment().add(12, 'MM').valueOf();
     const token = jwt.encode({
         iss: user.uid,
-        exp: expires,
         payload: user
     }, config.env.token.secret);
 	
