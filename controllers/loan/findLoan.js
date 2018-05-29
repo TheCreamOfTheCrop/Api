2cf4f86056c25ad817f5c7b8ef8db1056644c2a3
ThomasEcalle <thomasecalle@hotmail.fr>
2018-05-30 00:51:31 +0200

    looking for lon even if we are provider

--- controllers/loan/findLoan.js
+++ controllers/loan/findLoan.js
@@ -1,60 +1,63 @@
 const Loan = require('../../models/mysql/loan');
 
 module.exports = function(user_id,state_id){
-	
+
     let loan;
     if (!user_id) {
         return Promise.reject(new Error('user id is required'));
     }
-	
-	if (!state_id) {
-		
-		return Loan.findAll({
-        where: {
-			$or: [
-           { user_requester_id: user_id },
-		   { user_provider_id: user_id}
-			]
-        },
-    })
-	.then(l => loan = l)
+
+    if (!state_id) {
+
+        return Loan.findAll({
+            where: {
+                $or: [
+                    { user_requester_id: user_id },
+                    { user_provider_id: user_id}
+                ]
+            },
+        })
+            .then(l => loan = l)
     .then(handleFindLoanResponse)
-	.then(findLoanWithoutFilter)
-    
-		function handleFindLoanResponse(loan) {
-			if (!loan) {
-				return Promise.reject(new Error("Loans was not found"));
-			} 			 
-		}
-		
-		function findLoanWithoutFilter() {
-	        return loan;
-		}
-	}
-	
-	else {
-		
-		return Loan.findAll({
-        where: {
-            user_requester_id: user_id,
-            state_id: state_id
-        },
-    })
-	.then(l => loan = l)
+            .then(findLoanWithoutFilter)
+
+        function handleFindLoanResponse(loan) {
+            if (!loan) {
+                return Promise.reject(new Error("Loans was not found"));
+            }
+        }
+
+        function findLoanWithoutFilter() {
+            return loan;
+        }
+    }
+
+    else {
+
+        return Loan.findAll({
+            where: {
+                $or: [
+                    { user_requester_id: user_id },
+                    { user_provider_id: user_id}
+                ],
+                state_id: state_id
+            },
+        })
+            .then(l => loan = l)
     .then(handleFindLoanResponse)
-	.then(findLoanWithFilter)
-    
-		function handleFindLoanResponse(loan) {
-			if (!loan) {
-				return Promise.reject(new Error("Loans was not found"));
-			}    
-		}
-		
-		function findLoanWithFilter() {
-			return loan;
-		}
-	
-	}
-    
-    
+            .then(findLoanWithFilter)
+
+        function handleFindLoanResponse(loan) {
+            if (!loan) {
+                return Promise.reject(new Error("Loans was not found"));
+            }
+        }
+
+        function findLoanWithFilter() {
+            return loan;
+        }
+
+    }
+
+
 }
