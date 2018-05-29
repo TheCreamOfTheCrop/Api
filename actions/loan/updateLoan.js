9ba0d9f4371f8abad3260e697d61eef16e19f8ca
ThomasEcalle <thomasecalle@hotmail.fr>
2018-05-29 21:32:31 +0200

    fixed update route

--- actions/loan/updateLoan.js
+++ actions/loan/updateLoan.js
@@ -3,19 +3,19 @@ const loanController = require('../../controllers/loan');
 
 module.exports = function(req, res) {
     var loanUpdate = {};
-	let loan_id;
-	
-	if (!req.body.id_loan) {
+    let loan_id;
+
+    if (!req.body.id_loan) {
         res
-        .status(config.constants.BAD_REQUEST)
-        .json({
-            message: "missing the loan id",
-            success: false
-        })
+            .status(config.constants.BAD_REQUEST)
+            .json({
+                message: "missing the loan id",
+                success: false
+            })
     }
-	
-	loan_id = req.body.id_loan;
-    
+
+    loan_id = req.body.id_loan;
+
     if (req.body.amount) {
         loanUpdate.amount = req.body.amount;
     }
@@ -28,22 +28,28 @@ module.exports = function(req, res) {
     if (req.body.delay) {
         loanUpdate.delay = req.body.delay;
     }
+    if (req.body.loan_type) {
+        loanUpdate.loan_type = req.body.loan_type;
+    }
+    if (req.body.user_provider_id) {
+        loanUpdate.user_provider_id = req.body.user_provider_id;
+    }
 
     loanController.updateLoan(loan_id, loanUpdate)
-    .then((result) => {
+        .then((result) => {
         res
         .status(config.constants.OK)
         .json({
             message: "Successful update",
             success: true
         });
-    })
-    .catch((err) => {
+})
+.catch((err) => {
         res
         .status(config.constants.BAD_REQUEST)
         .json({
             message:err.message || err,
             success: false
         });
-    });
+});
 };
