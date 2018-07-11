5b9c60ec384cf2bc5d3cc2ac8d08b3d07e42c22f
ThomasEcalle <thomasecalle@hotmail.fr>
2018-07-11 14:25:34 +0200

    Add average note to User when adding a note

--- actions/note/add.js
+++ actions/note/add.js
@@ -1,33 +1,34 @@
 const config = require('../../config');
 const NoteController = require('../../controllers/note');
+const UserController = require('../../controllers/user');
 
-module.exports = function(req, res) {
-	const id = req.user.id;
+module.exports = function (req, res) {
+    const id = req.user.id;
     if (!req.body.note) {
         res
-        .status(config.constants.BAD_REQUEST)
-        .json({
-            message: "Missing note",
-            success: false
-        })
+            .status(config.constants.BAD_REQUEST)
+            .json({
+                message: "Missing note",
+                success: false
+            })
     }
-	
-	  if (!req.body.comments) {
+
+    if (!req.body.comments) {
         res
-        .status(config.constants.BAD_REQUEST)
-        .json({
-            message: "Missing comments",
-            success: false
-        })
+            .status(config.constants.BAD_REQUEST)
+            .json({
+                message: "Missing comments",
+                success: false
+            })
     }
-	
-	if (!req.body.user_id) {
+
+    if (!req.body.user_id) {
         res
-        .status(config.constants.BAD_REQUEST)
-        .json({
-            message: "Missing the user id",
-            success: false
-        })
+            .status(config.constants.BAD_REQUEST)
+            .json({
+                message: "Missing the user id",
+                success: false
+            })
     }
 
     if (!req.body.loan_id) {
@@ -41,25 +42,61 @@ module.exports = function(req, res) {
 
     const note = req.body.note;
     const comments = req.body.comments;
-	const id_provider = req.body.user_id;
-	const loan_id = req.body.loan_id;
+    const id_provider = req.body.user_id;
+    const loan_id = req.body.loan_id;
 
 
     NoteController.add(note, comments, id, id_provider, loan_id)
         .then((note) => {
-        res
-        .status(config.constants.OK)
-        .json({
-            note,
-            success: true
-        });
+
+            NoteController.listNote(id_provider)
+                .then((notes) => {
+
+
+                    var total = 0;
+                    console.log('starting moy calcul, notes = ' + JSON.stringify(notes));
+
+                    notes.forEach(function (element) {
+                        console.log('in loop note : ' + element.note);
+
+                        total += element.note;
+                    });
+
+                    let moy = total / notes.length;
+
+                    UserController.update(id_provider, {note: moy})
+                        .then((user) => {
+                            res.status(config.constants.OK)
+                                .json({
+                                    note,
+                                    success: true
+                                });
+                        })
+                        .catch((err) => {
+                            res
+                                .status(config.constants.BAD_REQUEST)
+                                .json({
+                                    message: err.message || err,
+                                    success: false
+                                });
+                        });
+                })
+                .catch((err) => {
+                    res
+                        .status(config.constants.BAD_REQUEST)
+                        .json({
+                            message: err.message || err,
+                            success: false
+                        });
+                });
+
         })
         .catch((err) => {
             res
-            .status(config.constants.BAD_REQUEST)
-            .json({
-                message:err.message || err,
-                success: false
-            });
+                .status(config.constants.BAD_REQUEST)
+                .json({
+                    message: err.message || err,
+                    success: false
+                });
         });
 };
